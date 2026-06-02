9f0f48494ec4d23aa1f4d2dbb4c933c0a83b7d78
Your Name <you@example.com>
2026-06-02 11:30:14 +0000

    tanzanite: Initial vendor bringup with 6.12 kernel modules

---
+++ Android.mk
@@ -0,0 +1,21 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),tanzanite)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,42c6a07807fefb358e605890f84c2a22b891ccfc)
+$(call add-radio-file-sha1-checked,radio/gz.img,a86006ac0ac7ee5ff8848250670b874740de7938)
+$(call add-radio-file-sha1-checked,radio/lk.img,9607388ffec2af15e2a18167069e05954b897257)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,d95369fba46dbaf565fa3916e2c9abbb9c9dd6a2)
+$(call add-radio-file-sha1-checked,radio/md1img.img,ede6bd76595a08aa683c646ea98fce343b1583c9)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,6951b5da984db0105c65667aced446b4cc8a93bd)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,4249cb286a9ccdf360b01f11e49d7014ee2e2208)
+$(call add-radio-file-sha1-checked,radio/scp.img,4ded70c940f99c8002c74f27181238330c77989f)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,1840bd80c2dca408c7b5504e74216689cca4ccd9)
+$(call add-radio-file-sha1-checked,radio/sspm.img,8f95b8337ec894b70fb27d791c1133dfa64bd4b1)
+$(call add-radio-file-sha1-checked,radio/tee.img,a0f9fc2e88d0c7479eb963586d9e4cdaf7833645)
+
+endif
